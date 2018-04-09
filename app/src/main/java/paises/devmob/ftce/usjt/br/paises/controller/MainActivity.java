b6f95988409c8167bea332663229e03a528e4dc3
Thiagobarbosa28 <thg.barbosa28@gmail.com>
2018-04-09 00:05:33 -0300

    Initial commit

---
+++ app/src/main/java/paises/devmob/ftce/usjt/br/paises/controller/MainActivity.java
@@ -0,0 +1,56 @@
+package paises.devmob.ftce.usjt.br.paises.controller;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import paises.devmob.ftce.usjt.br.paises.R;
+import paises.devmob.ftce.usjt.br.paises.model.Paises;
+import paises.devmob.ftce.usjt.br.paises.model.PaisesNetwork;
+
+public class MainActivity extends Activity {
+    public static final String PAIS = "paises.devmob.ftce.usjt.br.paises";
+    private EditText txtFila;
+    Context contexto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtFila = (EditText)findViewById(R.id.buscar_fila);
+        contexto = this;
+    }
+    public void buscarPaises(View view) {
+        String fila = txtFila.getText().toString();
+        new DownloadJsonChamados().execute("http://10.70.11.162:8080/arqsw_sdesk_a4_solucao_parcial/rest/chamados");
+
+    }
+
+
+    private class DownloadJsonChamados extends AsyncTask<String, Void, ArrayList<Paises>> {
+
+        @Override
+        protected ArrayList<Paises> doInBackground(String... strings) {
+            ArrayList<Paises> paises = new ArrayList<>();
+            try {
+                paises = PaisesNetwork.buscarPaises(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return paises;
+        }
+    }
+    protected void onPostExecute(ArrayList<Paises> paises){
+        Intent intent = new Intent(contexto, ListarPaisesActivity.class);
+        intent.putExtra(PAIS, paises);
+        startActivity(intent);
+    }
+}
+
